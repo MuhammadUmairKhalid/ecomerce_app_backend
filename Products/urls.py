6ed9ec7b55b1c51f7cc1e8e82a7a56499db90e82
muhammad umair <umairkhalid1314@gmail.com>
2025-01-24 21:38:38 +0500

    remove the migration files

--- Products/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from Products.views import  ProductView,GetImage
-
-
-urlpatterns = [
-    path("products/", ProductView.as_view(), name="products"),
-    path('get-image/<str:filename>/',GetImage.as_view(),name="getimage"),    
-]
--- registration/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from .views import RegisterView, Login
-
-urlpatterns = [
-    path("register/", RegisterView.as_view(), name="register"),  # noqa     
-    path("login/", Login.as_view(), name="login"),  # noqa     
-]
